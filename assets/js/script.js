0b80747d3491eeef0ef58a2c1a0433f1341d56bc
Arjav5090 <arjavvirani123@gmail.com>
2024-02-24 15:38:55 -0500

    Initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,7 +63,7 @@ document.addEventListener('visibilitychange',
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
-            document.title = "Come Back To Portfolio";
+            document.title = "Thank You for Visiting";
             $("#favicon").attr("href", "assets/images/favhand.png");
         }
     });
